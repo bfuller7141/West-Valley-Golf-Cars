8d0317b9e2621310dff01218bcf234a8d0f9999e
bfuller7141 <160172286+bfuller7141@users.noreply.github.com>
2026-01-26 10:10:07 -0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://example.com',
+  integrations: [react(), sitemap()],
 });
